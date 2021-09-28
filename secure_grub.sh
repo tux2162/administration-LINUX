93a12feee148ef3ca4496dea5144e8c418f493bf
MihranGIT <91540388+MihranGIT@users.noreply.github.com>
2021-09-28 14:06:00 +0200

    Update secure_grub.sh
    Ajout commentaire

--- secure_grub.sh
+++ secure_grub.sh
@@ -1,9 +1,10 @@
+# Création du mdp, modifier "root\nroot" par le password souhaité
 mdp=$(echo -e 'root\nroot' |grub-mkpasswd-pbkdf2|grep hachage|cut -d" " -f9)
+
+# Modification du fichier, modifier "root" par le nom d'utilisateur souhaité
 echo "cat << EOF" >> /etc/grub.d/00_header
 echo 'set superusers="root"' >> /etc/grub.d/00_header
 echo "password_pbkdf2 root $mdp" >> /etc/grub.d/00_header
 echo "EOF" >> /etc/grub.d/00_header
-echo 'GRUB_DISABLE_RECOVERY="true"' >> /etc/default/grub
-echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grubi
 update-grub
 
