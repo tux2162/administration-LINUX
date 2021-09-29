0b223a6e276cc2f8f28bf86d0ca599ba9fd6429c
MihranGIT <91540388+MihranGIT@users.noreply.github.com>
2021-09-29 20:16:18 +0200

    Update Script_Complet.sh
    Ajout interface utilisateur

--- Script_Complet.sh
+++ Script_Complet.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-
+# set -e
 
 apt install sudo -y
+sudo apt install figlet
 
-
+figlet Suppression des packages inutiles
 ### Supprimer tous les packages inutiles installés au départ
 sudo apt autoremove --purge
 sudo apt remove -y telnet
 sudo apt remove -y task-laptop
 
+sleep 2
+clear
+
+figlet Installation des packages utiles en prod
+
 ### Installer tous les packages utiles sur sur un serveur de prod
 apt install vim rsync screen mlocate btrfs-progs htop net-tools git tree gnupg2 mc psmisc lynx curl git cryptsetup pigz pixz zip ncdu iptraf iotop dstat gdisk mc cifs-utils ntfs-3g sshfs gdisk lshw inxi figlet screenfetch php php-fpm nginx mariadb-server -y
+
+clear
+
+figlet Generation cles ED25519
 ### Générer automatiquement les clés ED25519
 ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_root -C root
 ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_esgi -C esgi
 ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/id_davy -C davy
+
+sleep 2
+clear
+
+figlet Synchronisation horaire Chronyd
 
 ### Synchronisation horaire
 apt-get install chrony -y
@@ -29,6 +44,9 @@ echo "server 3.fr.pool.ntp.org iburst" >> /etc/chrony/chrony.conf
 # timedatectl set-ntp on
 # systemctl restart systemd-timesyncd
 # systemctl status systemd-tymesyncd --no-pager
+
+sleep 2
+clear
 
 #COFFRE
 #mot de passe du coffre = YES
@@ -47,9 +65,14 @@ mount -v /dev/mapper/NomLVChiffré home/esgi/COFFRE
 ### Mettre les fichiers de configs comme .bashrc, issue.net et motd
 # A FAIRE !!!!!
 
+figlet Vider le cache des packages
 ### Vider le cache des packages DEBIAN 
 apt-get clean
 
+sleep 2
+clean
+
+figlet Customization du shell
 ### Customization 
 # >> Modification du prompt pour le root
 echo "PS1='\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\n\[\e[0;93m\]\u\[\e[0m\]@\[\e[0;91m\]\h\n\[\e[0;38;5;246m\]\d\[\e[0m\] - \[\e[0;38;5;245m\]\t\n\[\e[0m\]Répertoire de travail : \[\e[0m\]\w\n\[\e[0m\]Version Bash : \[\e[0m\]\v\n\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\[\e[0m\]#\n\[\e[0m\]\W\[\e[0m\] \[\e[0m\]>\[\e[0m\] \[\e[0m\]'" >> /root/.bashrc
@@ -87,10 +110,19 @@ echo "alias grep='grep --color=auto'" >> ~/.bashrc
 # Chargement de la modification
 source ~/.bashrc
 
+sleep 2
+clear
+
+figlet Passage en résolution 1920x1080
+
 # Modification de la résolution
 sed -i s/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1920x1080/g /etc/default/grub
 update-grub
 
+sleep 2
+clear
+
+figlet Modification du Hostname et domaine
 # Modification du hostname
 echo "wiki" > /etc/hostname
 
@@ -102,9 +134,13 @@ sed -i 's/$variable/esgi.local/g' /etc/hosts
 #modification du domainame
 sed -i 's/localdomain/wiki.esgi.local/g' /etc/resolv.conf
 
+sleep 2
+clear
+
 ### Ajouter un user "ESGI" pouvoir root non chrooté, password:P@ssword, UID:10000, GID:10000
 # A FAIRE !!!!
 
+figlet Securisation du Grub
 
 ### Sécurisation du GRUB, création d'un compte root:root password en pbkdf2
 mdp=$(echo -e 'root\nroot' |grub-mkpasswd-pbkdf2|grep hachage|cut -d" " -f9)
@@ -114,10 +150,17 @@ echo "password_pbkdf2 root $mdp" >> /etc/grub.d/00_header
 echo "EOF" >> /etc/grub.d/00_header
 update-grub
 
+sleep 2
+clear
+
+figlet Desactivation option advance
 # Désactivé option avancé du GRUB
 echo 'GRUB_DISABLE_RECOVERY="true"' >> /etc/default/grub
 echo 'GRUB_DISABLE_SUBMENU=y' >> /etc/default/grub
 update-grub
+
+sleep 2
+clear
 
 echo "Reboot dans 3 secondes"
 sleep 3
