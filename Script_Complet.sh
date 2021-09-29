c560bc37779eb1b53dc4842fcefded9441b8ec84
tux2162 <56308568+tux2162@users.noreply.github.com>
2021-09-29 21:45:08 +0200

    Update Script_Complet.sh
    ajout des dossiers pour le coffre

--- Script_Complet.sh
+++ Script_Complet.sh
@@ -60,7 +60,17 @@ echo -e 'YES\n' |cryptsetup luksOpen /dev/VGCRYPT/lv_coffre COFFRE
 mkdir /home/esgi/COFFRE
 mkfs.btrfs  /dev/mapper/COFFRE
 mount -v /dev/mapper/COFFRE /home/esgi/COFFRE
-
+mkdir /home/esgi/COFFRE/CERTIFICAT
+mkdir /home/esgi/COFFRE/ENVIRONNEMENT
+mkdir /home/esgi/COFFRE/ENVIRONNEMENT/bash
+mkdir /home/esgi/COFFRE/ENVIRONNEMENT/ksh
+mkdir /home/esgi/COFFRE/ENVIRONNEMENT/zsh
+mkdir /home/esgi/COFFRE/MEMENTO
+mkdir /home/esgi/COFFRE/SECURITE
+mkdir /home/esgi/COFFRE/SECURITE/fail2ban
+mkdir /home/esgi/COFFRE/SECURITE/firewall
+mkdir /home/esgi/COFFRE/SECURITE/supervision
+mkdir /home/esgi/COFFRE/SERVEUR
 sleep 2
 
 ### Mettre en place la configuration réseau statique 
