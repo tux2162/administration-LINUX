394261611e4109e408c3052b470279dec0231c70
tux2162 <v.pinelli93@gmail.com>
2021-10-01 21:28:46 +0200

    avalider

--- Coffre/demontage.sh
+++ Coffre/demontage.sh
@@ -1,2 +1,9 @@
+if [ "$var" == "mysql" ]; then
+		
+		service mariadb stop
+fi
 fusermount -u /home/esgi/COFFRE
-cryptsetup luksClose /dev/mapper/COFFRE && umount -v /home/esgi/COFFRE
+cryptsetup luksClose /dev/mapper/COFFRE 
+var="$(ps aux |grep mariadb|grep mysql|cut -c1-5)"
+
+
