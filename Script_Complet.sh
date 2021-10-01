2cea5fcf54ee7e22543c4a222eddfadece66b45e
MihranGIT <leokhatchadourian@gmail.com>
2021-10-01 11:50:57 +0200

    Creation des dossiers

--- Script_Complet.sh
+++ Script_Complet.sh
@@ -54,29 +54,6 @@ echo "server 3.fr.pool.ntp.org iburst" >> /etc/chrony/chrony.conf
 sleep 2
 clear
 
-figlet Creation du coffre
-#COFFRE
-#mot de passe du coffre = YES
-lvcreate -L 2GB -n lv_coffre VGCRYPT
-echo -e 'YES\n' |cryptsetup luksFormat /dev/VGCRYPT/lv_coffre
-echo -e 'YES\n' |cryptsetup luksOpen /dev/VGCRYPT/lv_coffre COFFRE
-mkdir /home/esgi/COFFRE
-mkfs.btrfs  /dev/mapper/COFFRE
-mount -v /dev/mapper/COFFRE /home/esgi/COFFRE
-sleep 1
-mkdir /home/esgi/COFFRE/CERTIFICAT
-mkdir /home/esgi/COFFRE/ENVIRONNEMENT
-mkdir /home/esgi/COFFRE/ENVIRONNEMENT/bash
-mkdir /home/esgi/COFFRE/ENVIRONNEMENT/ksh
-mkdir /home/esgi/COFFRE/ENVIRONNEMENT/zsh
-mkdir /home/esgi/COFFRE/MEMENTO
-mkdir /home/esgi/COFFRE/SECURITE
-mkdir /home/esgi/COFFRE/SECURITE/fail2ban
-mkdir /home/esgi/COFFRE/SECURITE/firewall
-mkdir /home/esgi/COFFRE/SECURITE/supervision
-mkdir /home/esgi/COFFRE/SERVEUR
-cryptsetup luksClose /dev/mapper/COFFRE && umount -v /home/esgi/COFFRE
-sleep 2
 
 ### Mettre en place la configuration réseau statique 
 # A FAIRE !!!!!
