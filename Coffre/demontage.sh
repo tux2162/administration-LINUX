79275cf18503b037f88a81c43dbf2d058f035a78
tux2162 <56308568+tux2162@users.noreply.github.com>
2021-10-01 21:32:37 +0200

    Update demontage.sh

--- Coffre/demontage.sh
+++ Coffre/demontage.sh
@@ -1,9 +1,10 @@
+var="$(ps aux |grep mariadb|grep mysql|cut -c1-5)"
 if [ "$var" == "mysql" ]; then
 		
 		service mariadb stop
 fi
 fusermount -u /home/esgi/COFFRE
 cryptsetup luksClose /dev/mapper/COFFRE 
-var="$(ps aux |grep mariadb|grep mysql|cut -c1-5)"
+
 
 
