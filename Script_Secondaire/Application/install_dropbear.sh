4a8fcf45a42a794d68a0408b014bb3f9cf6e58d7
tux2162 <56308568+tux2162@users.noreply.github.com>
2021-10-27 10:01:03 +0200

    Update install_dropbear.sh

--- Script_Secondaire/Application/install_dropbear.sh
+++ Script_Secondaire/Application/install_dropbear.sh
@@ -1,23 +1,17 @@
-################
-##installation##
-################
+clear
+cd /tmp
 
-apt update -y && apt upgrade -y
-apt install dropbear nginx php php-fpm mariadb-server php-mysql fail2ban -y
+apt-get update  > /dev/null 2>&1
 
-###############
-#configuration#
-###############
+apt-get -y install dropbear ssh
+sed -i 's/NO_START\=1/NO_START\=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT\=22/DROPBEAR_PORT\=442/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS\=/DROPBEAR_EXTRA_ARGS\=\"\-p 442 \-p 80 \-p 8080 \-p 8484 \-p 143 \-p 109\"/g' /etc/default/dropbear
+sed -i 's/#PermitRootLogin prohibit\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin without\-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service dropbear start
+service sshd restart
 
-#dropbear
-sed -i "s/DROPBEAR_OPTIONS=/DROPBEAR_OPTIONS='-I 180 -j -k -p 2222 -s'/g " /etc/dropbear-initramfs/config
-#-I 180 : Disconnect the session if no traffic is transmitted or received in 180 seconds.
-#-j : Disable ssh local port forwarding.
-#-k : Also disable remote port forwarding.
-#-p 2222 : Listen Dropbear ssh server on specified address and TCP port. If just a port such as 2222 
-#is given listen on all addresses. up to 10 can be specified (default 22 if none specified).
-
-
-#mariadb
-#nginx
-#bookstack
+sleep 5 ; reboot
